f23c113796c8a16183de246c8bf7d16d35a6d7d8
severus889700 <severus889700@gmail.com>
2018-10-23 21:29:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,7 +40,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board)==true || draw?(board)==true || full?(board)==true
+  if won?(board)==true || draw?(board)==true || full?(board)==true || full?(board)==false
     return true
   elsif won?(board)==false
     return true
