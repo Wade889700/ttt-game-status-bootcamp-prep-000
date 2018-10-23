a0f6b7538dae21e575d9cee014b95a60121b79c1
severus889700 <severus889700@gmail.com>
2018-10-23 22:24:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
     if position_1=="X" && position_2=="X"&& position_3=="X" || position_1=="O" && position_2=="O"&& position_3=="O"
       return win_combination
     end
-  end
-else
-  false
+    end
+  else
+    false
 end
 
 def full?(board)
